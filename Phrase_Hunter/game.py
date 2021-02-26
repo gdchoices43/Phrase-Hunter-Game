16268f7bbb3b2795c2985546ced6e5833619ed0f
Tyler Duvall <gdchoices43@gmail.com>
2021-02-26 15:59:03 -0800

    Update to game.py
    Getting the easy stuff out of the way.

--- Phrase_Hunter/game.py
+++ Phrase_Hunter/game.py
@@ -11,21 +11,38 @@ class Game:
             Phrase("tech degree project three"),
             Phrase("object oriented python")
         ]
-        self.activate_phrase = None
-        self.guesses = 0
+        self.activate_phrase = self.get_random_phrase()
+        self.guesses = [" "]
 
     def start(self):
-        pass
-
+        self.welcme()
 
     def get_random_phrase(self):
         pass
 
     def welcome(self):
-        pass
+        print("""
+        +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+        Welcome To The Phrase Hunter Game!!
+        =+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=
+        """)
+        print("You are given 5 lives, each missed guess is 1 life.")
 
     def get_guess(self):
         pass
 
     def game_over(self):
-        pass
+                print("""\n
+        +=+=+=+=+=+=+=+=+=+=+=+=+=+
+        - - - - -GAME OVER- - - - -
+        =+=+=+=+=+=+=+=+=+=+=+=+=+=
+        """)
+        play_again = input("Would you like to pay again? Enter Y or N: ")
+        if play_again.lower() == "Y":
+            Game.start()
+        elif play_again.lower() == "N":
+            print("Hope you enjoyed the game! Exiting game...")
+            sys.exit()
+        else:
+            print("That was not a valid response. Try Again!")
+            print(play_again)
